3bb5f541bfd1361baaf9e77b88f6090cc16487ac
Sinu Sekhar <sinu_sekhar2@intuit.com>
2020-04-20 12:57:30 -0700

    first commit

---
+++ api/src/main/java/com/sinu/play/apps/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sinu.play.apps.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello, how are you?";
+    }
+}
